9e8caa375edcdc4d24dc90aacd5eb16953da51c2
Obada Yehia <67144995+obadatech01@users.noreply.github.com>
2023-06-02 10:30:48 +0300

    added paymentRoute

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const authRoute = require('./authRoute');
 const clientRoute = require('./clientRoute');
 const expenseRoute = require('./expenseRoute');
 const caseRoute = require('./caseRoute');
+const paymentRoute = require('./paymentRoute');
 const documentRoute = require('./documentRoute');
 const sessionRoute = require('./sessionRoute');
 const homeRoute = require('./homeRoute');
@@ -14,6 +15,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/clients', clientRoute);
   app.use('/api/v1/expenses', expenseRoute);
   app.use('/api/v1/cases', caseRoute);
+  app.use('/api/v1/payments', paymentRoute);
   app.use('/api/v1/documents', documentRoute);
   app.use('/api/v1/sessions', sessionRoute);
   app.use('/api/v1/home', homeRoute);
